26847292c9610296502a5a20054781b507728511
StylezZz <a20202692@pucp.edu.pe>
2025-04-10 23:04:29 -0500

    first commit

---
+++ src/main/java/pucp/edu/glp/glpdp1/Glpdp1Application.java
@@ -0,0 +1,13 @@
+package pucp.edu.glp.glpdp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Glpdp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Glpdp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/pucp/edu/glp/glpdp1/Glpdp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pucp.edu.glp.glpdp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Glpdp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
