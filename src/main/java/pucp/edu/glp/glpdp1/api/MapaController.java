5b24677f19b21ffe70e401f768013674a44d7586
rodrigo-loayza <loayza.rs@pucp.edu.pe>
2025-04-21 02:42:55 -0500

    feat: get pedidos

--- src/main/java/pucp/edu/glp/glpdp1/api/MapaController.java
+++ src/main/java/pucp/edu/glp/glpdp1/api/MapaController.java
@@ -2,10 +2,7 @@ package pucp.edu.glp.glpdp1.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import pucp.edu.glp.glpdp1.domain.Mapa;
 import pucp.edu.glp.glpdp1.service.MapaService;
@@ -34,5 +31,10 @@ public class MapaController {
         } catch (IOException e) {
             return ResponseEntity.badRequest().body("Error al cargar pedidos: " + e.getMessage());
         }
+    }
+
+    @GetMapping("/pedidos")
+    public ResponseEntity<?> obtenerPedidos() {
+        return ResponseEntity.ok(mapa.getPedidos());
     }
 }
